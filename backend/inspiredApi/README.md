5987bc8c62ef0d9edcbf16ce9096b6665b912339
西森大介 <nishimori.daisuke@di-v.co.jp>
2019-05-15 16:24:10 +0900

    a

--- backend/inspiredApi/README.md
+++ backend/inspiredApi/README.md
@@ -3,8 +3,11 @@
 
 # Build
 stack run
-
 or 
-
 stack build
 stack exec inspiredApi-exe
+
+
+# Layout
+## Modeling
+### Artist
--- backend/inspiredApi/app/Main.hs
+++ backend/inspiredApi/app/Main.hs
@@ -9,12 +9,14 @@ import Web.Scotty
 
 import User
 
+import Database.Bolt
 
 
 routes :: ScottyM()
 routes = do
   get "/users" fetchAllUsers
   get "/users/:id" fetchUser
+
 
 fetchAllUsers :: ActionM()
 fetchAllUsers = do
@@ -25,9 +27,13 @@ fetchUser = do
   id <- param "id"
   json (filter (matchesId id) allUsers)
 
-
+runQuery :: ActionM()
+runQuery = do
+  pipe <- connect $ def { user = "neo4j", password = "44662535Bsk" }
+  json allUsers
 
 main :: IO ()
 main = do
   putStrLn  "Starging Server..."
   scotty 3000 routes
+
